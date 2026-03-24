43c77d90b018d649ddfad8ce306f1757adbd3e2e
project <harshitavatnala@gmail.com>
2026-03-24 21:03:38 +0530

    Added changes such that pyq's are available

--- papersdata.js
+++ papersdata.js
@@ -1,3 +1,45 @@
+const subjectsData = {
+    "First Year Engineering": {
+        "Semester 1": ["Applied Mathematics I", "Applied Physics I", "Applied Chemistry I", "Basic Electrical and Electronics Engineering", "Engineering Mechanics", "Professional and Communication Ethics"],
+        "Semester 2": ["Applied Mathematics II", "Semiconductor Physics", "Data Structure", "Elements of electrical systems", "Elements of Mechanical Engineering", "Elements of Telecommunication", "Engineering Materials", "Environmental Chemistry and Non-Conventional Energy Sources", "Introduction to Chemical Engineering", "Physics of Measurements and Sensors"]
+    },
+
+    "Computer Engineering": {
+        "Semester 3": ["Digital Logic and Computer Architecture", "Data Structure", "Discrete Structures and Graph Theory", "Engineering Mathematics III"],
+        "Semester 4": ["Analysis of Algorithm", "Database Management System", "Engineering Mathematics IV", "Operating System"]
+    },
+
+    "Information Technology": {
+        "Semester 3": ["Data Structure Analysis", "Database Management System", "Principle of Communication", "Engineering Mathematics III"],
+        "Semester 4": ["Automata Theory", "Computer Network and Network Design", "Engineering Mathematics IV", "Computer Organization and Architecture", "Operating System"]
+    },
+
+    "Data Engineering": {
+        "Semester 3": ["Digital Logic and Computer Architecture", "Data Structure", "Discrete Structures and Graph Theory", "Engineering Mathematics III"],
+        "Semester 4": ["Analysis of Algorithm", "Database Management System", "Engineering Mathematics IV", "Operating System"]
+    },
+
+    "Mechanical Engineering": {
+        "Semester 3": ["Engineering Mathematics III", "Material Metallurgy", "Thermodynamics", "Production Processes", "Strength of Materials"],
+        "Semester 4": ["CAD/CAM", "Engineering Mathematics IV", "Fluid Mechanics", "Industrial Electronics", "Kinematics of Machinery"]
+    },
+
+    "Civil Engineering": {
+        "Semester 3": ["Architectural Planning and Design of Buildings", "Building Materials and Construction", "Engineering Mathematics III", "Structural Analysis I", "Surveying I"],
+        "Semester 4": ["Building Services", "Engineering Mathematics IV", "Structural Analysis II", "Surveying II", "Transportation Engineering"]
+    },
+
+    "Electrical Engineering": {
+        "Semester 3": ["Engineering Mathematics III", "Electrical Circuit Analysis", "Electrical Machines I", "Electronic Devices and Circuits", "Signals and Systems"],
+        "Semester 4": ["Engineering Mathematics IV", "Electrical Machines II", "Control Systems", "Power Systems I", "Power Electronics"]
+    }
+};
+
+
+
+
+
+
 const papers = [
 
     // First Year Engineering - Semester 1
@@ -526,7 +568,7 @@ const papers = [
     {
         branch: "Data Engineering",
         semester: "Semester 3",
-        subjects: ["Data Structure"],
+        subjects: ["Data Structure Analysis"],
         year: "2024",
         month: "May-June",
         file: "papers/be_computer-engineering_semester-3_2024_may_data-structurerev-2019-c-scheme.pdf"
@@ -751,7 +793,7 @@ const papers = [
         file: "papers/be_mechanical-engineering_semester-3_2025_may_strength-of-materials-rev-2019-c-scheme.pdf"
     },
    
-    //Mechanical Engineering - Semester 3
+    //Mechanical Engineering - Semester 4
     {
         branch: "Mechanical Engineering",
         semester: "Semester 4",
@@ -841,4 +883,365 @@ const papers = [
         month: "May-June",
         file: "papers/be_mechanical-engineering_semester-4_2025_may_kinematics-of-machineryrev-2019-c-scheme.pdf"
     },
-]
+
+    //Civil Engineering - Semester 3
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Architectural Planning and Design of Buildings"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2024_may_architectural-planning-design-of-buildingsrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Fluid Mechanics"],    
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2024_may_fluid-mechanics-irev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Engineering Geology"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2024_may_engineering-geologyrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Engineering Mathematics III"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2024_may_engineering-mathematics-iiirev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Mechanics of Solids"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2024_may_mechanics-of-solidsrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Architectural Planning and Design of Buildings"],
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2025_may_architectural-planning-design-of-buildingsrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Enginneering Geology"],    
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2025_may_engineering-geologyrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Engineering Mathematics III"],       
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2025_may_engineering-mathematics-iiirev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 3",
+        subjects: ["Mechanics of Solids"],
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-3_2025_may_mechanics-of-solidsrev-2019-c-scheme.pdf"
+    },
+
+    
+    //Civil Engineering - Semester 4
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Building Materials and Concrete Technology"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2024_may_building-materials-concrete-technologyrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Engineering Mathematics IV"],    
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2024_may_engineering-mathematics-iv-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Structural Analysis"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2024_may_structural-analysisrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Surveying"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2024_may_surveyingrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Fluid Mechanics"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2023_may_fluid-mechanics-iirev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Building Materials and Concrete Technology"],
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2025_may_building-materials-concrete-technologyrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Engineering Mathematics IV"],    
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2025_may_engineering-mathematics-iv-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Fluid Mechanics"],       
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2025_may_fluid-mechanics-iirev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Structural Analysis"],       
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2025_may_structural-analysisrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Civil Engineering",
+        semester: "Semester 4",
+        subjects: ["Surveying"],
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_civil-engineering_semester-4_2025_may_surveyingrev-2019-c-scheme.pdf"
+    },
+
+    //Electronics Engineering - Semester 3
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Electrical Networks Analysis and Synthesis"],
+        year: "2024",
+        month: "Dec-Jan",
+        file: "papers/be_electronics-engineering_semester-3_2024_december_electrical-networks-analysis-and-synthesis-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Electronic Devices and Circuits I"],    
+        year: "2024",
+        month: "Dec-Jan",
+        file: "papers/be_electronics-engineering_semester-3_2024_december_electronic-devices-and-circuits-i-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Electronic Instruments and Measurements"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-3_2024_may_electronic-instruments-and-measurements-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Digital Logic Circuits"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-3_2024_may_digital-logic-circuits-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Electrical Networks Analysis and Synthesis"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-3_2024_may_electrical-networks-analysis-and-synthesis-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Engineering Mathematics III"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-3_2024_may_engineering-mathematics-iii-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Digital Logic Circuits"],    
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-3_2025_may_digital-logic-circuits-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Electronic devices and Circuits I"],       
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-3_2025_may_electronic-devices-and-circuits-i-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 3",
+        subjects: ["Engineering Mathematics III"],
+        year: "2025",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-3_2025_may_engineering-mathematics-iii-rev-2019-c-scheme.pdf"
+    },
+
+    
+    //Civil Engineering - Semester 4
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 4",
+        subjects: ["Electronics Devices and Circuits II"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-4_2024_may_electronics-devices-and-circuits-iirev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 4",
+        subjects: ["Engineering Mathematics IV"],    
+        year: "2024",
+        month: "Dec-Jan",
+        file: "papers/be_electronics-engineering_semester-4_2024_december_engineering-mathematics-rev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 4",
+        subjects: ["Microcontroller Applications"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-4_2024_may_microcontroller-applicationsrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 4",
+        subjects: ["Principles of Communication Engineering"],       
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-4_2024_may_principles-of-communication-enggrev-2019-c-scheme.pdf"
+    },
+
+    {
+        branch: "Electronics Engineering",
+        semester: "Semester 4",
+        subjects: ["Signals and Systems"],
+        year: "2024",
+        month: "May-June",
+        file: "papers/be_electronics-engineering_semester-4_2024_may_signals-and-systems-rev-2019-c-scheme.pdf"
+    },
+
+];
+
+//Filter Function
+function updateSubjects() {
+    const branch = document.getElementById("branch").value;
+    const semester = document.getElementById("semester").value;
+    const subjectDropdown = document.getElementById("subject");
+
+    subjectDropdown.innerHTML = "<option>Select Subject</option>";
+
+    if (subjectsData[branch] && subjectsData[branch][semester]) {
+        subjectsData[branch][semester].forEach(subject => {
+            let option = document.createElement("option");
+            option.value = subject;
+            option.textContent = subject;
+            subjectDropdown.appendChild(option);
+        });
+    }
+}
+
+function filterPapers() {
+    const selectedSubject = document.getElementById("subject").value;
+    const table = document.querySelector("table");
+
+    //Remove old rows (except header)
+    table.innerHTML = `
+        <tr>
+            <th>Year</th>
+            <th>Month</th>
+            <th>Link</th>
+        </tr>
+    `;
+
+    let found = false;
+
+    papers.forEach(paper => {
+        if(paper.subjects.includes(selectedSubject)) {
+            found = true;
+
+            let row =`
+                <tr>
+                    <td>${paper.year}</td>
+                    <td>${paper.month}</td>
+                    <td><a href="${paper.file}" target="_blank">View Paper</a></td>
+                    </tr>
+                    `;
+            table.innerHTML += row;
+        }
+    });
+
+    if (!found) {
+        table.innerHTML += `
+            <tr>
+                <td colspan="3">No papers found for the selected subject.</td>
+            </tr>
+        `;
+    }
+}
